4dfad54cf6f4a44e4a77ff4ee9b4ac66020cfbfe
kumarvivek2022 <vivek.kumar@app.cbnits.com>
2023-02-16 14:31:19 +0530

    custom_updated

--- lib/screen/login_screen.dart
+++ lib/screen/login_screen.dart
@@ -8,9 +8,15 @@ class LoginScreen extends StatefulWidget {
   State<LoginScreen> createState() => _LoginScreenState();
 }
 
+bool isSwitchOn = true;
+bool isSwitched = true;
+
 class _LoginScreenState extends State<LoginScreen> {
-  bool isSwitchOn = true;
-  bool isSwitched = true;
+  final nameController = TextEditingController();
+  final phoneController = TextEditingController();
+  final genderController = TextEditingController();
+  final ageController = TextEditingController();
+  final locationController = TextEditingController();
   bool enableEdit = false;
   bool isShow = true;
   @override
@@ -21,44 +27,16 @@ class _LoginScreenState extends State<LoginScreen> {
               padding: const EdgeInsets.all(20),
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
+                children: const [
                   SizedBox(
                     width: 110,
                     height: 35,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        shape: const StadiumBorder(),
-                        primary: Colors.white,
-                        onPrimary: Colors.black,
-                        side: const BorderSide(
-                          color: Colors.black,
-                        ),
-                      ),
-                      child: const Text(
-                        'CANCEL',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ),
+                    child: CustomButton(title: 'CANCEL', backgrndColor: Colors.white, foregrndColor: Colors.black)
                   ),
                   SizedBox(
                     width: 110,
                     height: 35,
-                    child: ElevatedButton(
-                      onPressed: () {},
-                      style: ElevatedButton.styleFrom(
-                        shape: const StadiumBorder(),
-                        primary: Colors.green,
-                        onPrimary: Colors.black,
-                        side: const BorderSide(
-                          color: Colors.black,
-                        ),
-                      ),
-                      child: const Text(
-                        'SAVE',
-                        style: TextStyle(fontSize: 16),
-                      ),
-                    ),
+                    child: CustomButton(title: 'SAVE', backgrndColor: Colors.green, foregrndColor: Colors.black)
                   ),
                 ],
               ),
@@ -87,8 +65,7 @@ class _LoginScreenState extends State<LoginScreen> {
       body: Padding(
         padding: const EdgeInsets.only(left: 18, right: 18, top: 5),
         child: SingleChildScrollView(
-          child: Stack(
-            children: [
+          child:
               Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
@@ -110,127 +87,76 @@ class _LoginScreenState extends State<LoginScreen> {
                     height: 20,
                   ),
                   Center(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.white, width: 3.0),
-                        color: Colors.white.withOpacity(0.2),
-                        shape: BoxShape.circle,
-                        boxShadow: const [
-                          BoxShadow(
-                              blurRadius: 20,
-                              color: Colors.grey,
-                              spreadRadius: 4)
-                        ],
-                      ),
-                      child: const CircleAvatar(
-                        radius: 50,
-                        backgroundImage:
-                            AssetImage('assets/images/emmanda.png'),
-                      ),
+                    child: Stack(
+                      children: [
+                        Container(
+                          width: 300,
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.white, width: 3.0),
+                            color: Colors.white,
+                            shape: BoxShape.circle,
+                            boxShadow: const [
+                              BoxShadow(
+                                  blurRadius: 10,
+                                  color: Colors.grey,
+                                  )
+                            ],
+                          ),
+                          child: const CircleAvatar(
+                            radius: 50,
+                            backgroundImage:
+                                AssetImage('assets/images/emmanda.png',),
+                          ),
+                        ),
+                        Positioned(
+                            top: 50,
+                            right: 80,
+                            child: Container(
+                              decoration: BoxDecoration(
+                                border: Border.all(color: Colors.white, width: 3),
+                                shape: BoxShape.circle,
+                              ),
+                              child: InkWell(
+                                onTap: () {},
+                                child: const CircleAvatar(
+                                  backgroundColor: Colors.green,
+                                  child: Icon(
+                                    Icons.edit,
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            )),
+
+                      ],
                     ),
                   ),
                   const SizedBox(
                     height: 30,
                   ),
-                 customTextField('Full Name','Dor Alex'),
-                  customTextField('Phone Number','+91727771548'),
-                  customTextField('Gender', 'Male'),
-                  customTextField('Age', '24'),
-                  customTextField('Location', 'New Town'),
-
+                  customTextField('Full Name', 'Dor Alex',nameController),
+                  customTextField('Phone Number', '+91727771548',phoneController),
+                  customTextField('Gender', 'Male',genderController),
+                  customTextField('Age', '24',ageController),
+                  customTextField('Location', 'New Town',locationController),
                   const SizedBox(
                     height: 20,
                   ),
-
                   if (enableEdit) ...[
-                    Row(
-                      children: const [
-                        Icon(
-                          Icons.notification_add_outlined,
-                          color: Colors.green,
-                          size: 30,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          "Notifications",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                      ],
+                    const CustomHeading(
+                      headingTitle: 'Notification',
+                      headingIcon: Icons.notification_add_outlined,
                     ),
-                    const Divider(
-                      color: Colors.black12,
-                      thickness: 1,
-                    ),
-                    const SizedBox(
-                      height: 10,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Push notifications",
-                          style: TextStyle(
-                              fontSize: 17, color: Colors.grey.withOpacity(1)),
-                        ),
-                        FlutterSwitch(
-                          value: isSwitchOn,
-                          activeColor: Colors.green,
-                          height: 25,
-                          width: 45,
-                          onToggle: (value) {
-                            setState(() {
-                              isSwitchOn = value;
-                            });
-                          },
-                        ),
-                      ],
-                    ),
-                    const SizedBox(
-                      height: 20,
-                    ),
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text(
-                          "Email notifications",
-                          style: TextStyle(
-                              fontSize: 17, color: Colors.grey.withOpacity(1)),
-                        ),
-                        FlutterSwitch(
-                          value: isSwitched,
-                          activeColor: Colors.green,
-                          height: 25,
-                          width: 45,
-                          onToggle: (value) {
-                            setState(() {
-                              isSwitched = value;
-                            });
-                          },
-                        ),
-                      ],
+                    const CustomNotification(
+                      subTitle1: 'Push notifications',
+                      subTitle2: 'Email notifications',
                     ),
                     const SizedBox(
                       height: 25,
                     ),
-                    Row(
-                      children: const [
-                        Icon(
-                          Icons.person_outline,
-                          color: Colors.green,
-                          size: 30,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          "Support",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                      ],
+                    const CustomHeading(
+                      headingTitle: 'Support',
+                      headingIcon: Icons.person_outline,
                     ),
                     const Divider(
                       color: Colors.black12,
@@ -255,23 +181,7 @@ class _LoginScreenState extends State<LoginScreen> {
                     const SizedBox(
                       height: 25,
                     ),
-                    Row(
-                      children: const [
-                        Icon(
-                          Icons.security,
-                          color: Colors.green,
-                          size: 30,
-                        ),
-                        SizedBox(
-                          width: 10,
-                        ),
-                        Text(
-                          "Security",
-                          style: TextStyle(
-                              fontSize: 20, fontWeight: FontWeight.bold),
-                        ),
-                      ],
-                    ),
+                    const CustomHeading(headingTitle: 'Security',headingIcon: Icons.security,),
                     const Divider(
                       color: Colors.black12,
                       thickness: 1,
@@ -290,44 +200,14 @@ class _LoginScreenState extends State<LoginScreen> {
                     const SizedBox(
                       height: 20,
                     ),
-                    Center(
-                      child: ElevatedButton(
-                        onPressed: () {},
-                        style: ElevatedButton.styleFrom(
-                          shape: const StadiumBorder(),
-                          primary: Colors.white,
-                          onPrimary: Colors.black,
-                          side: const BorderSide(
-                            color: Colors.black,
-                          ),
-                        ),
-                        child: const Text(
-                          'SIGN OUT',
-                          style: TextStyle(fontSize: 16),
-                        ),
+                    const Center(
+                      child: CustomButton(title: 'SIGN OUT', backgrndColor: Colors.white, foregrndColor: Colors.black,
+
                       ),
-                    ),
+                    )
                   ],
                 ],
               ),
-              Positioned(
-                  top: 140,
-                  right: 110,
-                  child: Container(
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.white, width: 3),
-                      shape: BoxShape.circle,
-                    ),
-                    child: const  CircleAvatar(
-                      backgroundColor: Colors.green,
-                      child: Icon(
-                        Icons.edit,
-                        color: Colors.white,
-                      ),
-                    ),
-                  )),
-            ],
-          ),
         ),
       ),
     );
@@ -351,6 +231,7 @@ class _LoginScreenState extends State<LoginScreen> {
       ],
     );
   }
+
   Widget security(String title, void Function()? onPressed) {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -369,56 +250,154 @@ class _LoginScreenState extends State<LoginScreen> {
       ],
     );
   }
-  Widget customTextField(String title,String text){
-    return  Column(
+
+  Widget customTextField(String title, String text,dynamic mycontroller) {
+    return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         Text(
           title,
-          style:const  TextStyle(
-              fontSize: 17,
-              fontWeight: FontWeight.bold,
-              color: Colors.grey),
+          style: const TextStyle(
+              fontSize: 17, fontWeight: FontWeight.bold, color: Colors.grey),
         ),
         TextField(
+          controller: mycontroller,
           readOnly: enableEdit,
-          decoration:  InputDecoration(
+          decoration: InputDecoration(
             hintText: text,
           ),
         ),
         const SizedBox(
           height: 20,
         ),
-
       ],
     );
   }
-  Widget customDivider(title,customIcon){
+}
+
+class CustomHeading extends StatelessWidget {
+  final dynamic headingIcon;
+  final String headingTitle;
+  const CustomHeading({super.key, this.headingIcon, required this.headingTitle});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Icon(
+          headingIcon,
+          color: Colors.green,
+          size: 30,
+        ),
+        const SizedBox(
+          width: 10,
+        ),
+        Text(
+          headingTitle,
+          style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+        ),
+      ],
+    );
+  }
+}
+
+class CustomNotification extends StatefulWidget {
+  final String subTitle1;
+  final String subTitle2;
+
+  const CustomNotification({
+    super.key,
+    required this.subTitle1,
+    required this.subTitle2,
+  });
+
+  @override
+  State<CustomNotification> createState() => _CustomNotificationState();
+}
+
+class _CustomNotificationState extends State<CustomNotification> {
+  @override
+  Widget build(BuildContext context) {
     return Column(
       children: [
-        Row(
-          children:  [
-            Icon(
-              customIcon,
-              color: Colors.green,
-              size: 30,
-            ),
-            SizedBox(
-              width: 10,
-            ),
-            Text(
-              title,
-              style: TextStyle(
-                  fontSize: 20, fontWeight: FontWeight.bold),
-            ),
-          ],
-        ),
         const Divider(
           color: Colors.black12,
           thickness: 1,
         ),
+        const SizedBox(
+          height: 10,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              widget.subTitle1,
+              style: TextStyle(fontSize: 17, color: Colors.grey.withOpacity(1)),
+            ),
+            FlutterSwitch(
+              value: isSwitchOn,
+              activeColor: Colors.green,
+              height: 25,
+              width: 45,
+              onToggle: (value) {
+                setState(() {
+                  isSwitchOn = value;
+                });
+              },
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              widget.subTitle2,
+              style: TextStyle(fontSize: 17, color: Colors.grey.withOpacity(1)),
+            ),
+            FlutterSwitch(
+              value: isSwitched,
+              activeColor: Colors.green,
+              height: 25,
+              width: 45,
+              onToggle: (value) {
+                setState(() {
+                  isSwitched = value;
+                });
+              },
+            ),
+          ],
+        ),
       ],
-
     );
   }
 }
+
+class CustomButton extends StatelessWidget{
+  final String title;
+  final dynamic backgrndColor;
+  final dynamic foregrndColor;
+  const CustomButton({super.key, required this.title, required this.backgrndColor, required this.foregrndColor});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: () {},
+      style: ElevatedButton.styleFrom(
+        shape: const StadiumBorder(),
+        backgroundColor: backgrndColor,
+        foregroundColor: foregrndColor,
+        side: const BorderSide(
+          color: Colors.black,
+        ),
+      ),
+      child:  Text(
+        title,
+        style: const TextStyle(fontSize: 16),
+      ),
+    );
+
+  }}
+
